7087cec4225fc2458c94626e62261c3e0fdeb16b
dionisioR <72356766+dionisioR@users.noreply.github.com>
2025-02-25 13:11:41 -0300

    Upload 2025-02-25   13h11
    Upload 2025-02-25   13h11

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -7,6 +7,7 @@ use bng\Models\Agents;
 
 class Main extends BaseController
 {
+  //==============================================
   public function index()
   {
     // check if there os no active user in session
@@ -52,29 +53,48 @@ class Main extends BaseController
     }
 
     // check if there was a post request
-    if($_SERVER["REQUEST_METHOD"] != "POST"){
+    if ($_SERVER["REQUEST_METHOD"] != "POST") {
       $this->index();
       return;
     }
 
     // form validation
     $validation_errors = [];
-    if(empty($_POST["text_username"]) || empty($_POST["text_password"])){
+    if (empty($_POST["text_username"]) || empty($_POST["text_password"])) {
       $validation_errors[] = "Username e Password são obrigatórios!";
-    }
-
-    // check if there are validation errors
-    if(!empty($validation_errors)){
-      $_SESSION["validation_errors"] = $validation_errors;
-     $this->login_frm();
-      return;
     }
 
     // ger form data
     $username = $_POST["text_username"];
     $password = $_POST["text_password"];
 
+    // check if username is valid email and between 5 and 50 characters
+    if (!filter_var($username, FILTER_VALIDATE_EMAIL)) {
+      $validation_errors[] = "O username tem de ser um email válido!";
+    }
+
+    // check if username is between 5 and 50 characters
+    if (strlen($username) < 5 || strlen($username) > 50) {
+      $validation_errors[] = "O username tem de ter entre 5 e 50 caracteres!";
+    }
+
+    // check if password is valid
+    if(strlen($password) < 6 || strlen($password) > 12){
+      $validation_errors[] = "A password deve ter entre 6 e 12 caracteres!";
+    }
+
+    // check if there are validation errors
+    if (!empty($validation_errors)) {
+      $_SESSION["validation_errors"] = $validation_errors;
+      $this->login_frm();
+      return;
+    }
+
+
+
     echo "Username: $username<br>";
     echo "Password: $password<br>";
   }
+
+  //==============================================
 }
