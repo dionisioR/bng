0d16330776544685eda1c3988c3a0a7ac9085107
dionisioR <72356766+dionisioR@users.noreply.github.com>
2025-02-28 13:02:57 -0300

    Update 2025-02-28   13h02
    Update 2025-02-28   13h02

--- app/views/homepage.php
+++ app/views/homepage.php
@@ -15,7 +15,7 @@
 
             <!-- adicionar clientes -->
             <?php if ($user->profile == 'agent'): ?>
-                <a href="#" class="unlink m-2">
+                <a href="?ct=agent&mt=new_client_frm" class="unlink m-2">
                     <div class="home-option p-5 text-center">
                         <h3 class="mb-3"><i class="bi bi-person-plus-fill"></i></h3>
                         <h5>Adicionar clientes</h5>
--- app/views/layouts/html_header.php
+++ app/views/layouts/html_header.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <!DOCTYPE html>
 <html lang="pt">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -18,5 +19,12 @@
     <link rel="stylesheet" href="assets/fontawesome/all.min.css">
     <!-- custom css -->
     <link rel="stylesheet" href="assets/app.css">
+
+    <?php if (isset($flatpickr)): ?>
+        <!-- flatpickr -->
+        <link rel="stylesheet" href="assets/flatpickr/flatpickr.min.css">
+        <script src="assets/flatpickr/flatpickr.js"></script>
+    <?php endif; ?>
 </head>
+
 <body>
